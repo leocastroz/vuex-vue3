aa94758f7bb85183fb7cd9510ebba534460901ba
leocastroz <castroz.dev@gmail.com>
2023-05-04 17:04:43 -0300

    Crete: create style simple in div.

---
+++ src/components/MinWallpaper.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+
+<style scoped>
+
+div {
+    width: 100%;
+    height: 120px;
+    background-image: url(../assets/min-wallpaper.svg);
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-clip: content-box;
+    border-radius: 10px 10px 0 0;
+}
+
+</style>
